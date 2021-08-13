add7495ab738e232313fea0d906841b07ec2f9fa
Harshita Mengji <72075214+harshitamengji@users.noreply.github.com>
2021-08-13 14:17:31 +0530

    Operators

---
+++ Operators1.py
@@ -0,0 +1,48 @@
+#Arithmetic operators
+x=3
+y=6
+z=1
+print("\n x+y= ",x+y)
+print("\n x-z= ",x-z)
+print("\n z*y= ",z*y)
+print("\n x/y= ",x/y)
+print("\n z*y= ",z*y)
+
+#Comparison  operators
+p=6
+f=8
+print("\n p>f=",p>f)
+print("\n p<f=",p<f)
+print("\n p!=f",p!=f)
+
+#Logical operators
+a=True
+b=False
+print("\na and b is",a and b)
+print("\na or b is",a or b)
+print("\nnot b is",not b)
+
+#Identity operators
+
+ x1=5
+ x2="Hello"
+ x3=[1,2,3]
+
+ y1=5
+ y2="Hello"
+ y3=[1,2,3]
+
+ print("x1 is not y2",x1 is not y2)
+ print("x1 is not y1",x1 is not y1)
+ print("x2 is y2",x2 is y2)
+
+
+ #membership Operators
+
+ m1="Hello World"
+ m2=[1:'a',2:'b']
+
+ print('H' in m1)
+ print('Hello' not in m1)
+ print(1 in m2)
+ print('a'in m2)
