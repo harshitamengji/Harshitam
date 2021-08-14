7a28a8cd1d5dc964177a8d6a0573e8ffa766430b
Harshita Mengji <72075214+harshitamengji@users.noreply.github.com>
2021-08-14 14:58:05 +0530

    Tuple

---
+++ Tuple Data.py
@@ -0,0 +1,12 @@
+#Write a program to manipulate Tuple data
+B=("a","b","c","d","e","f","g","h","i","j")
+
+print("Tuple B:",B)
+
+print("Tuple B: 2 to 5",B[2:6])
+
+print("Tuple B in reverse:",B[::-1])
+
+print("Count of a is :",B.count('a'))
+
+print("Index of a",B.index('a'))
